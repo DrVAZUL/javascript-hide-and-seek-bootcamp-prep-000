a8da4f87894d52d3b2732fb8b3dcb4e2e8a96c5e
Max Zelinka <fourthwalljoke@gmail.com>
2017-11-09 19:44:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,9 +12,10 @@ return document.getElementById('grand-node').querySelector('div div div div div'
 
 function increaseRankBy(n) {
   var list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
-
+  var array = new Array()
   for(var i = 0; i < list.length; i++) {
     list[i].innerHTML = (parseInt(list[i].innerHTML) + n).toString()
-    return list.innerHTML
+    array.push(list[i].innerHTML)
   }
+  return array 
 }
