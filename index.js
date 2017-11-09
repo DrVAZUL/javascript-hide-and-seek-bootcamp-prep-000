3bbf2bd584838ae300768088b29550e1964a65d5
Max Zelinka <fourthwalljoke@gmail.com>
2017-11-09 18:57:45 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function getFirstSelector(Selector) {
+return document.querySelector(Selector)
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target')
+}
+
+function deepestChild() {
+return document.getElementById('grand-node').querySelector('div div div div div')
+}
+
+function increaseRankBy(n) {
+  debugger
+  var list = document.getElementbyId('app').querySelectorAll('ul.ranked-list li')
+
+  for(var i = 0; i < list.length; i++) {
+    list.innerHTML[i] = (parseInt(list.innerHTML[i]) + n).toString()
+    return list.innerHTML
+  }
+}
